844896bcedb879ae742c3d597ce3d7b2f919a07e
Gorkiy Chocolate <beybagame@gmail.com>
2025-09-12 02:17:42 +0500

    https://www.youtube.com/watch?v=-PX_4nEE7p8 https://www.youtube.com/watch?v=A4uNotdECBg
    Project code was wrote from Youtube guide and adapted by me.
    I learned and understand factory and abstract factory pattern from Youtube guide

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,182 @@
 //https://www.youtube.com/watch?v=-PX_4nEE7p8
+//https://www.youtube.com/watch?v=A4uNotdECBg
 
 public class Main {
     public static void main(String[] args) {
-        Factory laptopsfactory = new AbstractFactory().createFactory("Laptops");
+        Factory Laptopfactory = new AbstractFactory().createFactory("Laptop");
         Factory pcfactory = new AbstractFactory().createFactory("PersonalComputer");
 
-        Laptops gaming = laptopsfactory.createLaptops("Gaming");
-        Laptops ultrabook = laptopsfactory.createLaptops("UltraBook");
+        Laptop gaming = Laptopfactory.createLaptop("Gaming");
+        Laptop ultrabook = Laptopfactory.createLaptop("UltraBook");
+        Laptop netbook = Laptopfactory.createLaptop("NetBook");
+        Laptop transformerlaptop = Laptopfactory.createLaptop("TransformerLaptop");
 
-        gaming.power();
-        ultrabook.power();
+        System.out.println("Laptop factory created:");
+        System.out.println("Gaming Laptop:");
+
+        gaming.cpu();
+        gaming.gpu();
+        gaming.os();
+        gaming.selled();
+
+        System.out.println("UltraBook:");
+
+        ultrabook.cpu();
+        ultrabook.gpu();
+        ultrabook.os();
+        ultrabook.selled();
+
+        System.out.println("NetBook:");
+
+        netbook.cpu();
+        netbook.gpu();
+        netbook.os();
+        netbook.selled();
+
+        System.out.println("TransformerLaptop:");
+
+        transformerlaptop.cpu();
+        transformerlaptop.gpu();
+        transformerlaptop.os();
+        transformerlaptop.selled();
+        
+        System.out.println("PersonalComputer factory created:");
 
         PersonalComputer gamingpc = pcfactory.createPersonalComputers("Gaming");
         PersonalComputer officepc = pcfactory.createPersonalComputers("Office");
+        PersonalComputer monoblock = pcfactory.createPersonalComputers("Monoblock");
 
-        gamingpc.power();
-        officepc.power();
+        System.out.println("Gaming PC:");
+
+        gamingpc.cpu();
+        gamingpc.gpu();
+        gamingpc.os();
+        gamingpc.selled();
+
+        System.out.println("Office PC:");
+
+        officepc.cpu();
+        officepc.gpu();
+        officepc.os();
+        officepc.selled();
+
+        System.out.println("Monoblock:");
+
+        monoblock.cpu();
+        monoblock.gpu();
+        monoblock.os();
+        monoblock.selled();
 
     }
 }
 
 
 
+interface Laptop{
+    void cpu();
+    void gpu();    
+    void os();
+    void selled();
+    
+}
+class Gaming implements Laptop{
+    @Override
+    public void cpu() {
+        System.out.println("CPU has been installed in Gaming Laptop");
+    }
 
-interface Laptops{
-    void power();
-}
-class Gaming implements Laptops{
     @Override
-    public void power() {
-        System.out.println("Gaming Laptops power");
+    public void gpu() {
+        System.out.println("GPU has been installed in Gaming Laptop");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Gaming Laptop");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("Gaming Laptop in selling");
     }
 }
-class UltraBook implements Laptops{
+class UltraBook implements Laptop{
     @Override
-    public void power() {
-        System.out.println("UltraBook power");
+    public void cpu() {
+        System.out.println("CPU has been installed in Ultrabook");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU hasn't installed in Ultrabook");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Ultrabook");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("UltraBook in selling");
     }
 }
-class LaptopsFactory implements Factory{
+
+class NetBook implements Laptop{
     @Override
-    public Laptops createLaptops(String typeOfLaptops) {
-        switch (typeOfLaptops){
+    public void cpu() {
+        System.out.println("CPU has been installed in Netbook");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU hasn't installed in Netbook");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Netbook");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("Netbook in selling");
+    }
+}
+
+class TransformerLaptop implements Laptop{
+    @Override
+    public void cpu() {
+        System.out.println("CPU has been installed in TransformerLaptop");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU hasn't installed in TransformerLaptop");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in TransformerLaptop");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("TransformerLaptop in selling");
+    }
+}
+
+
+
+
+class LaptopFactory implements Factory{
+    @Override
+    public Laptop createLaptop(String typeOfLaptop) {
+        switch (typeOfLaptop){
             case "Gaming" : return new Gaming();
             case "UltraBook" : return new UltraBook();
+            case "NetBook" : return new NetBook();
+            case "TransformerLaptop" : return new TransformerLaptop();
             default: return null;
         }
     }
@@ -53,39 +186,97 @@ class LaptopsFactory implements Factory{
     }
 }
 
-
-
 interface PersonalComputer{
-    void power();
+    void cpu();
+    void gpu();
+    void os();
+    void selled();
 }
+
 class GamingPC  implements PersonalComputer{
     @Override
-    public void power() {
-        System.out.println("Gaming PC power");
+    public void cpu() {
+        System.out.println("CPU has been installed in Gaming PC");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU has been installed in Gaming PC");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Gaming PC ");
+    }
+    
+    @Override
+    public void selled() {
+        System.out.println("Gaming PC in selling");
     }
 }
+
 class OfficePc implements PersonalComputer{
     @Override
-    public void power() {
-        System.out.println("Office PC power");
+    public void cpu() {
+        System.out.println("CPU has been installed in Office PC");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU has been installed in Office PC OFF");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Office PC");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("Office PC in selling");
     }
 }
+
+
+class Monoblock implements PersonalComputer{
+    @Override
+    public void cpu() {
+        System.out.println("CPU has been installed in Monoblock");
+    }
+
+    @Override
+    public void gpu() {
+        System.out.println("GPU has been installed in Monoblock");
+    }
+
+    @Override
+    public void os() {
+        System.out.println("OS was installed in Monoblock");
+    }
+
+    @Override
+    public void selled() {
+        System.out.println("Monoblock in selling");
+    }
+}
+
 class PersonalComputersFactory implements Factory {
     public PersonalComputer createPersonalComputers(String typeOfPC) {
         switch (typeOfPC){
             case "Gaming" : return new GamingPC();
             case "Office" : return new OfficePc();
+            case "Monoblock" : return new Monoblock();
             default: return null;
         }
     }
     @Override
-    public Laptops createLaptops(String typeOfPC) {
+    public Laptop createLaptop(String typeOfPC) {
         return null;
     }
 }
 
 interface Factory {
-    Laptops createLaptops(String typeOfLaptops);
+    Laptop createLaptop(String typeOfLaptop);
     PersonalComputer createPersonalComputers(String typeOfPersonalComputers);
 }
 
@@ -94,7 +285,7 @@ interface Factory {
 class AbstractFactory{
     Factory createFactory(String typeOfFactory){
         switch (typeOfFactory){
-            case "Laptops" : return new LaptopsFactory();
+            case "Laptop" : return new LaptopFactory();
             case "PersonalComputer" : return new PersonalComputersFactory();
             default: return null;
         }
