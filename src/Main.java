e68c3ea9598a65da1cdd84c9881e1db6c55fd2ba
Gorkiy Chocolate <beybagame@gmail.com>
2025-09-25 19:32:41 +0500

    Assignment 3 code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MacOS macUser = new MacUser(); // объект MacOS
+        Windows11 programming = new VirtualMachine(macUser);
+        App app = new App();
+        app.ios(programming);
+    }
+}
+
+interface Windows11 {
+    void getProgramming();
+}
+
+interface MacOS {
+    void getSwift();
+}
+
+class VirtualMachine implements Windows11 {
+    MacOS macOS;
+
+    public VirtualMachine(MacOS macOS) {
+        this.macOS = macOS;
+    }
+
+    @Override
+    public void getProgramming() {
+        macOS.getSwift();
+    }
+}
+
+class MacUser implements MacOS {
+    @Override
+    public void getSwift() {
+        System.out.println("Programming on Swift");
+    }
+}
+
+class App {
+    public void ios(Windows11 programming) {
+        programming.getProgramming();
     }
 }
