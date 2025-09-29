be3218a4b9b9827eeba3f2f02810bfb12761c29a
Gorkiy Chocolate <beybagame@gmail.com>
2025-09-30 00:02:32 +0500

    Assignment 4, carcas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
+interface Laptop {
+    void doSomething();
+}
+
+class WindowsLaptop implements Laptop {
+    @Override
+    public void doSomething() {
+        System.out.println("Windows OS");
+    }
+}
+
+class MacLaptop implements Laptop {
+    @Override
+    public void doSomething() {
+        System.out.println("macOS");
+    }
+}
+
+
+abstract class TypeOfLaptop {
+    protected Laptop laptop;
+
+    public TypeOfLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public abstract void doSomething();
+}
+
+
+class GamingLaptop extends TypeOfLaptop {
+    public GamingLaptop(Laptop laptop) {
+        super(laptop);
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("Playing Game");
+        laptop.doSomething();
+    }
+}
+
+class UltrabookLaptop extends TypeOfLaptop {
+    public UltrabookLaptop(Laptop laptop) {
+        super(laptop);
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("Studying");
+        laptop.doSomething();
+    }
+}
+
+class OfficeLaptop extends TypeOfLaptop {
+    public OfficeLaptop(Laptop laptop) {
+        super(laptop);
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("Working");
+        laptop.doSomething();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        TypeOfLaptop gamingWin = new GamingLaptop(new WindowsLaptop());
+        gamingWin.doSomething();
+
+        TypeOfLaptop ultraMac = new UltrabookLaptop(new MacLaptop());
+        ultraMac.doSomething();
     }
 }
